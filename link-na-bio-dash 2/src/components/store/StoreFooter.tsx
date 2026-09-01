afdc792dd026301019d7d3a4758d4a561adccc4f
giuliagargione-sys <giuliagargione@uol.com.br>
2026-09-01 13:51:05 -0300

    Delete link-na-bio-dash 2 directory

--- link-na-bio-dash 2/src/components/store/StoreFooter.tsx
+++
@@ -1,9 +0,0 @@
-import type { StoreConfig } from "@/types/config";
-
-export function StoreFooter({ config }: { config: StoreConfig }) {
-  return (
-    <footer className="border-t border-border py-8 text-center text-sm text-muted-foreground">
-      {config.footer.copyText}
-    </footer>
-  );
-}
